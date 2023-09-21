b3432bf4603288466da03ddd2ad2b22e2d0e8dc7
Moro Bamber <mwbamber@alaska.edu>
2023-09-21 10:07:47 -0800

    evaluators testing with design pattern

--- libs/evaluator_calculus.py
+++ libs/evaluator_calculus.py
@@ -1,6 +1,12 @@
 from libs.evaluator_base import EvaluatorBase
+import sympy as sp
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
 
 class EvaluateCalculus(EvaluatorBase):
 
-    def evaluate(self):
-        print("execute method: EvaluateCalculus")
+    def evaluate(expression,operation,variable): 
+        parsed_in = parse_expr(expression,transformations='all')
+        if operation == 1: #differentiation
+            return diff(sp.sympify(parsed_in),variable) #degree of differentation possibly too
+        #operation 2 integration?
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+from libs.evaluator_workflow import evalWorkflow
+from libs.evaluator_arithmatic import EvaluateArithmatic
+
+#mode = 1
+mode = 2
+
+exprStr = "2+2"
+exprStr1 = "3x**2"
+
+evalWorkMode1 = evalWorkflow(1)
+evalWorkMode2 = evalWorkflow(2)
+
+result = evalWorkMode1.getEvaluator().evaluate(exprStr)
+result1 = evalWorkMode2.getEvaluator().evaluate(exprStr1,1,'x')
+
+print(result)
+print(result1)
