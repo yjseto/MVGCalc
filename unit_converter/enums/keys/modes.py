14136ee5792691c00e0f9afbe36359dbc0f24b45
Moro Bamber <99629944+UnderYourSpell@users.noreply.github.com>
2023-10-13 16:35:18 -0800

    great progress on the math and units for unit converting!

---
+++ unit_converter/enums/keys/modes.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class metric(Enum):
+    TEMPERATURE         = 1
+    SPEED               = 2
+    LENGTH              = 3
+    AREA                = 4
+    WEIGHT              = 5
