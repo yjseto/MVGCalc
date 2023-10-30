55d4618c89425317bf6d9e5ea630d8b7b549b93d
Moro Bamber <99629944+UnderYourSpell@users.noreply.github.com>
2023-10-30 14:55:19 -0800

    deleted stuff from local dev branch

--- commons/enums/constants/math_constants.py
+++
@@ -1,9 +0,0 @@
-import math
-from enum import Enum
-
-class Constants(Enum):
-    PI          = (1,"math.pi")
-    EULERSNUM   = (2,"math.e")
-    TAU         = (3,"math.tau")
-    INFINITY    = (4,"math.inf")
-    NAN         = (5,"math.nan")
--- libs/evaluator_algebra.py
+++
@@ -1,6 +0,0 @@
-from libs.evaluator_base import EvaluatorBase
-
-class EvaluateAlgebra(EvaluatorBase):
-
-    def evaluate(self):
-        print("execute method: EvaluateAlgebra")
